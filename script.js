310ca2bb5961d2cd695d2eb8e22bc4a195d0b469
CarolinaAndradeCunha <andradecunhacarolina@gmail.com>
2025-06-17 13:35:11 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-confirmar").addEventListener("click", function () {
+  document.getElementById("mensagem").textContent = "¡Te esperamos allá!";
+  document.getElementById("comidas").classList.remove("escondido");
+});
